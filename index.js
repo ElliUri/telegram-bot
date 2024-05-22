8a01cc45a6fdc1c37e0d1325e6ebcdbc529521a3
Eldana <fineshine6718@gmail.com>
2024-05-22 19:48:05 +0600

    fixed error

--- index.js
+++ index.js
@@ -1,78 +1,76 @@
 const TelegramBot = require('node-telegram-bot-api');
-const token = "";
+const token = "7081425896:AAHqMCP4ZcKRq9Oof_zvT2FdSKgVqMlB4Ro";
 // add token from bot father
 const bot = new TelegramBot(token, { polling: true });
 
 const quizQuestions = [
-  {
-    question: "Какой будет результат выполнения `console.log(typeof null)` в JavaScript?",
-    options: [
-      { text: 'Object', isCorrect: true },
-      { text: 'Null', isCorrect: false },
-      { text: 'Undefined', isCorrect: false }
-    ]
-  },
-  {
-    question: "Какой язык используется для стилизации веб-страниц?",
-    options: [
-      { text: 'HTML', isCorrect: false },
-      { text: 'CSS', isCorrect: true },
-      { text: 'JavaScript', isCorrect: false }
-    ]
-  },
-  {
-    question: "Какая компания разработала язык программирования JavaScript?",
-    options: [
-      { text: 'Microsoft', isCorrect: false },
-      { text: 'Netscape', isCorrect: true },
-      { text: 'Google', isCorrect: false }
-    ]
-  }
-];
+    {
+      question: "Какой будет результат выполнения `console.log(typeof null)` в JavaScript?",
+      options: [
+        { text: 'Object', isCorrect: true },
+        { text: 'Null', isCorrect: false },
+        { text: 'Undefined', isCorrect: false }
+      ]
+    },
+    {
+      question: "Какой язык используется для стилизации веб-страниц?",
+      options: [
+        { text: 'HTML', isCorrect: false },
+        { text: 'CSS', isCorrect: true },
+        { text: 'JavaScript', isCorrect: false }
+      ]
+    },
+    {
+      question: "Какая компания разработала язык программирования JavaScript?",
+      options: [
+        { text: 'Microsoft', isCorrect: false },
+        { text: 'Netscape', isCorrect: true },
+        { text: 'Google', isCorrect: false }
+      ]
+    }
+  ];
 
 const userStates = {};
 
 bot.onText(/\/start/, (msg) => {
-  const chatId = msg.chat.id;
-  userStates[chatId] = {
-    questionIndex: 0,
-    correctAnswers: 0,
-    incorrectAnswers: 0,
-  };
-  sendQuestion(chatId);
-});
+    const chatId = msg.chat.id;
+    userStates[chatId] = { questionIndex: 0, correctAnswers: 0, incorrectAnswers: 0 };
+    sendQuestion(chatId);
+  });
 
-function  sendQuestion(chatId) {
+  function sendQuestion(chatId) {
     const userState = userStates[chatId];
     const questionIndex = userState.questionIndex;
-    const questionData = quizQuestions[questionIndex]
-
+    const questionData = quizQuestions[questionIndex];
+    
     const options = {
-        reply_markup: {
-            inline_keyboard: questionData.options.map((option, index) => [
-                {text: option.text, callback_data: JSON.stringify({questionIndex, optionIndex: index})}
+      reply_markup: {
+        inline_keyboard: questionData.options.map((option, index) => [
+          { text: option.text, callback_data: JSON.stringify({ questionIndex, optionIndex: index }) }
         ])
-        }
+      }
     };
-
-    bot.sendMessage(chatId, questionData.question, options)
-}
-
+  
+    bot.sendMessage(chatId, questionData.question, options);
+  }
+  
 bot.on('callback_query', (query) => {
     const chatId = query.message.chat.id;
-    const {questionIndex, optionText} = JSON.parse(query.data);
+    const { questionIndex, optionIndex} = JSON.parse(query.data);
     const questionData = quizQuestions[questionIndex];
-    const selectedOption = questionData.options[optionText];
+    const selectedOption = questionData.options[optionIndex];
     const userState = userStates[chatId];
 
-    if(selectedOption,isCorrect) {
-        bot.sendMessage(chatId, 'Правильно')
+    if(selectedOption.isCorrect) {
+        // ничего не работало до этого я написала , а надо было .
+        bot.sendMessage(chatId, 'Правильно!')
         userState.correctAnswers++ 
     } else {
         bot.sendMessage(chatId, 'Неправильно!')
         userState.incorrectAnswers++
     }
 
+    
     userState.questionIndex++;
 
     if (userState.questionIndex < quizQuestions.length) {
@@ -80,6 +78,6 @@ bot.on('callback_query', (query) => {
     } else {
         bot.sendMessage(chatId, `Викторина завершена! Правильных ответов: ${userState.correctAnswers}, 
         Неправильных ответов: ${userState.incorrectAnswers}`)
-        delete userState[chatId];
+        delete userStates[chatId];
     }
 })
