0a764ecaea7af90486ba7ba1555ac68e69aa1721
Eldana <fineshine6718@gmail.com>
2024-05-22 19:15:59 +0600

    logic for bot

---
+++ index.js
@@ -0,0 +1,85 @@
+const TelegramBot = require('node-telegram-bot-api');
+const token = "";
+// add token from bot father
+const bot = new TelegramBot(token, { polling: true });
+
+const quizQuestions = [
+  {
+    question: "Какой будет результат выполнения `console.log(typeof null)` в JavaScript?",
+    options: [
+      { text: 'Object', isCorrect: true },
+      { text: 'Null', isCorrect: false },
+      { text: 'Undefined', isCorrect: false }
+    ]
+  },
+  {
+    question: "Какой язык используется для стилизации веб-страниц?",
+    options: [
+      { text: 'HTML', isCorrect: false },
+      { text: 'CSS', isCorrect: true },
+      { text: 'JavaScript', isCorrect: false }
+    ]
+  },
+  {
+    question: "Какая компания разработала язык программирования JavaScript?",
+    options: [
+      { text: 'Microsoft', isCorrect: false },
+      { text: 'Netscape', isCorrect: true },
+      { text: 'Google', isCorrect: false }
+    ]
+  }
+];
+
+const userStates = {};
+
+bot.onText(/\/start/, (msg) => {
+  const chatId = msg.chat.id;
+  userStates[chatId] = {
+    questionIndex: 0,
+    correctAnswers: 0,
+    incorrectAnswers: 0,
+  };
+  sendQuestion(chatId);
+});
+
+function  sendQuestion(chatId) {
+    const userState = userStates[chatId];
+    const questionIndex = userState.questionIndex;
+    const questionData = quizQuestions[questionIndex]
+
+    const options = {
+        reply_markup: {
+            inline_keyboard: questionData.options.map((option, index) => [
+                {text: option.text, callback_data: JSON.stringify({questionIndex, optionIndex: index})}
+        ])
+        }
+    };
+
+    bot.sendMessage(chatId, questionData.question, options)
+}
+
+bot.on('callback_query', (query) => {
+    const chatId = query.message.chat.id;
+    const {questionIndex, optionText} = JSON.parse(query.data);
+    const questionData = quizQuestions[questionIndex];
+    const selectedOption = questionData.options[optionText];
+    const userState = userStates[chatId];
+
+    if(selectedOption,isCorrect) {
+        bot.sendMessage(chatId, 'Правильно')
+        userState.correctAnswers++ 
+    } else {
+        bot.sendMessage(chatId, 'Неправильно!')
+        userState.incorrectAnswers++
+    }
+
+    userState.questionIndex++;
+
+    if (userState.questionIndex < quizQuestions.length) {
+        sendQuestion(chatId)
+    } else {
+        bot.sendMessage(chatId, `Викторина завершена! Правильных ответов: ${userState.correctAnswers}, 
+        Неправильных ответов: ${userState.incorrectAnswers}`)
+        delete userState[chatId];
+    }
+})
